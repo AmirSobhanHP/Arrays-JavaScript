f0421822d4ceb03592ab6818e4d1a004ac3b5406
AmirsobhanHP <persiandownload2021@gmail.com>
2021-09-04 18:38:36 +0430

    first commit

---
+++ JS-Array-Methods/Array-Methods.js
@@ -0,0 +1,19 @@
+let Persons = ["Amir","Mahdi","Ali","Hossein","Mohammad"];// Create Array
+console.log(Persons); // print Array
+// console.log(Persons[2]); // Print item in Array by index
+// Persons.push("Hamid"); // Add item to Array
+// Persons.pop(); // Remove item from end Array
+// Persons.shift(); // Remove item from first index of the Array
+// Persons.unshift("Parsa"); // Add item to first index of the Array
+// Persons.splice(3,4,"Parham","Danial"); // adds and/or removes array elements(start index,end index,*replace item)
+// console.log(Persons);
+// new_Persons = Persons.slice(2,5);
+// console.log(new_Persons);
+// console.log(Persons.reverse()); // reverse Array
+// console.log(Persons.sort()); // sort Array A-z
+// console.log(Persons.sort().reverse()); // sort Array Z-A
+// console.log(Persons.includes("Parsa")); // chek is there item in Array or not there
+// let Programmers = ["Ablofazl","Amirmahdy","AmirSobhan"];
+// Persons = Persons.concat(Programmers); // Add Array to end Array
+// console.log(Persons); 
+// console.log(Persons.indexOf("Hossein")); // print index of item.if item not in Array print -1
---
+++ Js-Array-slicing/Array-Slicing.js
@@ -0,0 +1,10 @@
+let Countries = [
+    ["Iran","Japan","China","Armenia"],
+    ["Italy","France","England","Germany"],
+    ["Usa","Canada","Mexico","Brazil"]
+]
+// console.log(Countries);
+// console.log(Countries[0]); log = ["Iran","Japan","China","Armenia"]
+// console.log(Countries[1]); log = ["Italy","France","England","Germany"]
+// console.log(Countries[2]); log = ["Usa","Canada","Mexico","Brazil"]
+// console.log(Countries[1][2]) log = "England"
